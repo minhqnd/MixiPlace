a506cfabeeeee7a8cc3df958e7c5cc6e0570082e
LiquidRekto <dungliqrekt@gmail.com>
2023-07-23 08:58:49 +0700

    final changes to the selenium screen

--- autoexec.py
+++ autoexec.py
@@ -1,4 +1,5 @@
 import selenium
+import os
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
@@ -7,9 +8,12 @@ from selenium.webdriver.common.by import By
 import threading
 import time
 from msedge.selenium_tools import Edge, EdgeOptions
+#import shutil
+#shutil.rmtree('D:\\Edge Data\\')
+#os.makedirs("D:\\Edge Data\\")
 
 
-credentials = [("user","pass")]
+credentials = [("usr","pass")]
 
 
 
@@ -20,6 +24,8 @@ import urllib.request
 def read_to_string(target_url):
     with urllib.request.urlopen(target_url) as response:
         return response.read().decode('utf-8').strip()
+    
+js = read_to_string("https://raw.githubusercontent.com/LiquidRekto/rplacevnhideout/master/us.js")
 
 def login(driver: webdriver.Edge | webdriver.Chrome | webdriver.Firefox, username: str, password: str):
     # Đăng nhập vào reddit
@@ -37,8 +43,10 @@ def login(driver: webdriver.Edge | webdriver.Chrome | webdriver.Firefox, usernam
         # ID của iframe này thay đổi sau mỗi reload, phải fetch lại
         time.sleep(5)
     
-        driver.execute_script(read_to_string("https://raw.githubusercontent.com/LiquidRekto/rplacevnhideout/master/us.js"))
+        driver.execute_script(js)
     except Exception as e:
+        driver.delete_all_cookies()
+
         print(e)
 
 
@@ -50,7 +58,15 @@ thread_list = list()
 for i in range(N):
     edge_options = EdgeOptions()
     edge_options.use_chromium = True
+    edge_options.add_experimental_option("detach", True)
     edge_options.add_argument("-inprivate")
+
+    # Set the path of the profile ending with User Data, not the profile folder
+    #edge_options.add_argument(f"--user-data-dir=D:\\Edge Data\\{i}")
+
+    # Specify the actual profile folder
+    #edge_options.add_argument(f"profile-directory=My_Profile_{i}")
+    edge_options.add_argument("--disable-dev-shm-usage")
     driver = Edge(options=edge_options)
     driver.get("https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2Fr%2Fplace%2F%3Fscreenmode%3Dfullscreen%26cx%3D168%26cy%3D245%26px%3D19")
     
